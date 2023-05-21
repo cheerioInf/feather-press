ca6ac030a250262acc9b63c234b49dca0f7e4282
CheerioInf <lltxsbsbg@gmail.com>
2023-05-21 20:10:15 +0800

    feat: 实现 toc 插件

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -6,6 +6,9 @@ import rehypeStringify from 'rehype-stringify';
 import { rehypePluginPreWrapper } from '../plugin-mdx/rehypePlugins/preWrapper';
 import { rehypePluginShiki } from '../plugin-mdx/rehypePlugins/shiki';
 import shiki from 'shiki';
+import remarkMdx from 'remark-mdx';
+import { remarkPluginToc } from '../plugin-mdx/remarkPlugins/toc';
+import remarkStringify from 'remark-stringify';
 
 describe('Markdown compile cases', async () => {
   // 初始化 processor
@@ -40,6 +43,30 @@ describe('Markdown compile cases', async () => {
     expect(result.value).toMatchInlineSnapshot(`
       "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre class=\\"shiki nord\\" style=\\"background-color: #2e3440ff\\" tabindex=\\"0\\"><code><span class=\\"line\\"><span style=\\"color: #D8DEE9\\">console</span><span style=\\"color: #ECEFF4\\">.</span><span style=\\"color: #88C0D0\\">log</span><span style=\\"color: #D8DEE9FF\\">(</span><span style=\\"color: #B48EAD\\">123</span><span style=\\"color: #D8DEE9FF\\">)</span></span>
       <span class=\\"line\\"></span></code></pre></div>"
+    `);
+  });
+
+  test('Compile TOC', async () => {
+    const mdContent = '## hello';
+    const remarkProssor = unified()
+      .use(remarkParse)
+      .use(remarkMdx)
+      .use(remarkPluginToc)
+      .use(remarkStringify);
+
+    const result = remarkProssor.processSync(mdContent);
+    expect(result.value.toString().replace(mdContent, ''))
+      .toMatchInlineSnapshot(`
+      "
+
+      export const toc = [
+        {
+          \\"id\\": \\"hello\\",
+          \\"text\\": \\"hello\\",
+          \\"depth\\": 2
+        }
+      ];
+      "
     `);
   });
 });
